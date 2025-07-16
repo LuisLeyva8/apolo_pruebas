55bdad0a5cf5e9f34f2d1d54b83e09106808facd
LuisLeyva8 <yolo_pez57@hotmail.com>
2025-07-16 16:56:11 -0600

    Commit inicial

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # apolo_pruebas
 apolo_pruebas
+# apolo_prueba
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/apolo_prueba/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.apolo_prueba
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
